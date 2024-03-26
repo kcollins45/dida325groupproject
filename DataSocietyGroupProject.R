1f7ff60874789fff845afea43f4e50d92e40d24f
Kevin Collins <kcollin9@binghamton.edu>
2024-03-26 10:58:13 -0400

    Added Initial File

---
+++ DataSocietyGroupProject.R
@@ -0,0 +1,9 @@
+#setwd("C:/Users/dante/Desktop/Data&Soc/Rural_Atlas_Update24")
+#getwd()
+
+County = read.csv("CountyClassifications.csv")
+Income = read.csv("Income.csv")
+Job = read.csv("Jobs.csv")
+People = read.csv("People.csv")
+Veterans = read.csv("Veterans.csv")
+VariableName = read.csv("VariableNameLookup.csv")
